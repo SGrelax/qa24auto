6ebdf655ef16d578bbdef93d22d34659df69f289
Artem <tsiuro.a@gmail.com>
2023-04-16 15:54:40 +0300

    task4

--- src/main/java/Homework2/Task4.java
+++ src/main/java/Homework2/Task4.java
@@ -1,6 +1,8 @@
 package Homework2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task4 {
@@ -8,5 +10,16 @@ public class Task4 {
         System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Chromedrive\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://uhomki.com.ua/");
+        WebElement hom = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[3]/div"));
+        WebElement hom2 = driver.findElement(By.xpath("/html/body/div[2]/footer/div/div/div/div[2]/div/div"));
+        WebElement hom3 = driver.findElement(By.xpath("/html/body/div[2]/footer/div/div/div/div[3]/div/div[1]"));
+        System.out.println(hom.getText());
+        System.out.println("-------------------------------------");
+        System.out.println(hom2.getText());
+        System.out.println("-------------------------------------");
+        System.out.println(hom3.getText());
+        System.out.println("-------------------------------------");
+
     }
 }
